ac697f546ede247933f44967004aa1f0197aaa62
brooksat <brooksat@brooksat-pc.wireless-1x.unc.edu>
2019-08-25 14:42:09 -0400

    Updated Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,38 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int count = scan.nextInt();
+		
+		String[] firstNames  = new String[count];
+		
+		String[] lastNames = new String[count];
+		
+		for (int i = 0; i < firstNames.length; i++) {
+			firstNames[i] = scan.next();
+			lastNames[i] = scan.next();
+			int foodCount = scan.nextInt();
+			
+			int[] itemCount = new int[foodCount];
+			String[] foodName = new String[foodCount];
+			double[] itemPrice = new double[foodCount];
+			for (int p = 0; p < itemCount.length; p++) {
+				itemCount[p] = scan.nextInt();
+				foodName[p] = scan.next();
+				itemPrice[p] = scan.nextDouble();
+			}
+			double total = calculateTotal(itemCount, itemPrice);
+			
+			System.out.println(firstNames[i].charAt(0) + ". " + lastNames[i] + ": " + total);
+		}
+		
+		
+	}
+	static double calculateTotal(int[] count, double[] price) {
+		double total = 0;
+		for (int i = 0; i < count.length; i++) {
+			double a = count[i] * price[i];	
+			total += a;
+		}
+		return total;
 	}
 }
